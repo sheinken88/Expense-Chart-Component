cfb59f2095cdc3388c863147f597b16da4e2fe2a
sheinken <heinken.sebastian@gmail.com>
2022-10-14 09:17:40 -0300

    new file:   .gitignore 	new file:   README-template.md 	modified:   README.md 	new file:   data.json 	new file:   design/active-states.jpg 	new file:   design/desktop-design.jpg 	new file:   design/desktop-preview.jpg 	new file:   design/mobile-design.jpg 	new file:   images/favicon-32x32.png 	new file:   images/logo.svg 	new file:   index.html 	new file:   main.js 	new file:   style-guide.md 	new file:   style.css

---
+++ main.js
@@ -0,0 +1,32 @@
+// Import JSON from data.json file
+const data = fetch("./data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    return data;
+  })
+  .catch((error) => console.log(error));
+
+let chartContainer = document.querySelector(".chart-container");
+
+// Map through data and populate chart
+data.then((data) => {
+  data.forEach((item) => {
+    let chart = document.createElement("div");
+    chart.classList.add("chart");
+    const height = item.amount / 7;
+    let value = "";
+    if (item.day == "wed") {
+      value = `<div class="chart-value active" style="--height:${height}em"></div>`;
+    } else {
+      value = `<div class="chart-value" style="--height:${height}em"></div>`;
+    }
+    chart.innerHTML = `
+        <div class="chart-wrapper">
+        ${value}
+        </div>
+        <div class="chart-title">${item.day}</div>
+        `;
+    chartContainer.appendChild(chart);
+  });
+});
